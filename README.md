3a2013a0345118c9dfecd6b6ed2bcf21cd355287
Jon Seay <jon@joncodes.com>
2024-10-04 17:52:40 -0400

    defined a mailSummary and a way to get them

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Imbox
-Imbox ~is~ will be a simple CLI tool for reading `.mbox` files. Inspired by a gmail export I did.
+Imbox ~is~ will be a simple TUI tool for reading `.mbox` files. Inspired by a gmail export I did.
 
 ## Running locally
-- `go run main.go`
+- `go run *.go open [path/to/mbox]`
--- mailbox/list.go
+++ mailbox/list.go
@@ -1,7 +1,29 @@
 package mailbox
 
-import "net/mail"
+import (
+	"net/mail"
+	"slices"
+	"time"
+)
 
-func GetList(emails []mail.Message, rangeStart int, rangeEnd int) []mail.Message {
+type mailSummary struct {
+	id      int
+	date    time.Time
+	subject string
+}
 
+func GetSummaryList(emails []mail.Message, rangeStart int, rangeEnd int) []mailSummary {
+	var summaries []mailSummary
+
+	for i := 0; i < len(emails); i++ {
+		date, _ := emails[i].Header.Date()
+		subject := emails[i].Header.Get("subject")
+		summaries = append(summaries, mailSummary{i, date, subject})
+	}
+
+	slices.SortFunc(summaries, func(a, b mailSummary) int {
+		return a.date.Compare(b.date)
+	})
+
+	return summaries
 }
